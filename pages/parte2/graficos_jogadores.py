cf8e83b016b7e284719ee92225d19a30eb6caa5b
pedronso <pedrocaetanoaraujopc@gmail.com>
2025-02-03 11:19:50 -0300

    parte 2 - ajuste dos gráficos

--- pages/parte2/graficos_jogadores.py
+++ pages/parte2/graficos_jogadores.py
@@ -13,18 +13,15 @@ def plot_distribution_altair(df, coluna, nome_estatistica):
     mediana = df[coluna].median()
     moda = df[coluna].mode()[0] if not df[coluna].mode().empty else None
 
-    # Criação condicional do DataFrame de estatísticas
     estatisticas = [
-        ('Média', media, 'blue'),
-        ('Mediana', mediana, 'orange')
+        (f'Média ({media:.2f})', media, 'blue'),
+        (f'Mediana ({mediana:.2f})', mediana, 'orange')
     ]
     if moda is not None:
-        estatisticas.append(('Moda', moda, 'green'))
+        estatisticas.append((f'Moda ({moda:.2f})', moda, 'green'))
 
-    # Convertendo a lista para um DataFrame
     linhas = pd.DataFrame(estatisticas, columns=['Estatística', 'Valor', 'Cor'])
 
-    # Gráfico de distribuição
     chart = alt.Chart(df).mark_bar(opacity=0.5, color='skyblue').encode(
         alt.X(coluna, bin=alt.Bin(maxbins=7), title=nome_estatistica),
         alt.Y('count()', title='Frequência')
@@ -32,7 +29,6 @@ def plot_distribution_altair(df, coluna, nome_estatistica):
         title=f'Distribuição de {nome_estatistica}'
     )
 
-    # Gráfico das linhas das estatísticas
     linhas_chart = alt.Chart(linhas).mark_rule().encode(
         x='Valor:Q',
         color=alt.Color('Estatística:N', scale=alt.Scale(domain=linhas['Estatística'], range=linhas['Cor'])),
@@ -40,7 +36,6 @@ def plot_distribution_altair(df, coluna, nome_estatistica):
         tooltip=['Estatística', 'Valor']
     )
 
-    # Combinando os gráficos
     st.altair_chart(chart + linhas_chart, use_container_width=True)
 
 def plot_box_plot_plotly(df, colunas):
